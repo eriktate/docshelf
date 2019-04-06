94e0215409376e82261e9e7585f491ae72a3f4f0
eriktate <erik.tate.r@gmail.com>
2019-04-06 13:35:32 -0400

    Resolving merge issues with http statuses

--- http/status.go
+++ http/status.go
@@ -1,40 +1,58 @@
 package http
 
-import "net/http"
+import (
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
 
 func serverError(w http.ResponseWriter, msg string) {
 	w.WriteHeader(http.StatusInternalServerError)
-	w.Write([]byte(msg))
+	if _, err := w.Write([]byte(msg)); err != nil {
+		log.WithError(err).Error()
+	}
 }
 
 func okJson(w http.ResponseWriter, data []byte) {
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+	if _, err := w.Write(data); err != nil {
+		log.WithError(err).Error()
+	}
 }
 
 func okHtml(w http.ResponseWriter, data []byte) {
-	w.Header().Add("Conent-Type", "text/html; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+	if _, err := w.Write(data); err != nil {
+		log.WithError(err).Error()
+	}
 }
 
 func badRequest(w http.ResponseWriter, msg string) {
 	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(msg))
+	if _, err := w.Write([]byte(msg)); err != nil {
+		log.WithError(err).Error()
+	}
 }
 
 func noContent(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNoContent)
-	w.Write(nil)
+	if _, err := w.Write(nil); err != nil {
+		log.WithError(err).Error()
+	}
 }
 
 func notAllowed(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusMethodNotAllowed)
-	w.Write(nil)
+	if _, err := w.Write(nil); err != nil {
+		log.WithError(err).Error()
+	}
 }
 
 func notFound(w http.ResponseWriter) {
 	w.WriteHeader(http.StatusNotFound)
-	w.Write(nil)
+	if _, err := w.Write(nil); err != nil {
+		log.WithError(err).Error()
+	}
 }
