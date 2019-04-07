664d0ac21a1c7b75f2837be0c2f5446c69d4dee1
eriktate <erik.tate.r@gmail.com>
2019-04-07 12:59:09 -0400

    Adding naive ListDocs functionality to dynamo backend along with relavent tests

--- dynamo/dynamo.go
+++ dynamo/dynamo.go
@@ -15,55 +15,28 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/dynamodbiface"
 	"github.com/eriktate/docshelf"
 	"github.com/eriktate/docshelf/env"
+	"github.com/pkg/errors"
 	"github.com/rs/xid"
 )
 
 const (
-	userTable = "ds_user"
-	docTable  = "ds_doc"
+	defUserTable = "ds_user"
+	defDocTable  = "ds_doc"
+	defTagTable  = "ds_tag"
 )
-
-func (s Store) userTableInput() dynamodb.CreateTableInput {
-	gsiKey := dynamodb.KeySchemaElement{
-		AttributeName: aws.String("email"),
-		KeyType:       dynamodb.KeyTypeHash,
-	}
-
-	gsi := dynamodb.GlobalSecondaryIndex{
-		IndexName:  aws.String(fmt.Sprintf("%s_email_idx", s.userTable)),
-		KeySchema:  []dynamodb.KeySchemaElement{gsiKey},
-		Projection: &dynamodb.Projection{ProjectionType: dynamodb.ProjectionTypeKeysOnly},
-	}
-
-	hashKey := dynamodb.KeySchemaElement{
-		AttributeName: aws.String("id"),
-		KeyType:       dynamodb.KeyTypeHash,
-	}
-
-	attrDef := []dynamodb.AttributeDefinition{
-		makeAttrDef("id", dynamodb.ScalarAttributeTypeS),
-		makeAttrDef("email", dynamodb.ScalarAttributeTypeS),
-	}
-
-	return dynamodb.CreateTableInput{
-		TableName:              aws.String(s.userTable),
-		BillingMode:            dynamodb.BillingModePayPerRequest,
-		AttributeDefinitions:   attrDef,
-		GlobalSecondaryIndexes: []dynamodb.GlobalSecondaryIndex{gsi},
-		KeySchema:              []dynamodb.KeySchemaElement{hashKey},
-	}
-}
 
 // A Store has methods that know how to interact with docshelf data in Dynamo.
 type Store struct {
 	client dynamodbiface.DynamoDBAPI
+	fs     docshelf.FileStore
 
 	userTable string
 	docTable  string
+	tagTable  string
 }
 
 // New creates a new Store struct.
-func New() (Store, error) {
+func New(fs docshelf.FileStore) (Store, error) {
 	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		return Store{}, err
@@ -74,7 +47,10 @@ func New() (Store, error) {
 
 	store := Store{
 		client:    svc,
-		userTable: env.GetEnvString("DS_DYNAMO_USER_TABLE", userTable),
+		fs:        fs,
+		userTable: env.GetEnvString("DS_DYNAMO_USER_TABLE", defUserTable),
+		docTable:  env.GetEnvString("DS_DYNAMO_DOC_TABLE", defDocTable),
+		tagTable:  env.GetEnvString("DS_DYNAMO_TAG_TABLE", defTagTable),
 	}
 
 	return store, store.ensureTables()
@@ -83,22 +59,8 @@ func New() (Store, error) {
 // GetUser fetches an existing docshelf User from dynamodb.
 func (s Store) GetUser(ctx context.Context, id string) (docshelf.User, error) {
 	var user docshelf.User
-	key, err := makeKey("id", id)
-	if err != nil {
-		return user, err
-	}
 
-	input := dynamodb.GetItemInput{
-		TableName: aws.String(s.userTable),
-		Key:       key,
-	}
-
-	res, err := s.client.GetItemRequest(&input).Send()
-	if err != nil {
-		return user, err
-	}
-
-	if err := dyna.UnmarshalMap(res.Item, &user); err != nil {
+	if err := s.getItem(ctx, s.userTable, "id", id, &user); err != nil {
 		return user, err
 	}
 
@@ -113,6 +75,7 @@ func (s Store) GetEmail(ctx context.Context, email string) (docshelf.User, error
 		return user, err
 	}
 
+	// TODO (erik): Figure out if this actually works with the GSI.
 	input := dynamodb.GetItemInput{
 		TableName: aws.String(s.userTable),
 		Key:       key,
@@ -194,6 +157,213 @@ func (s Store) RemoveUser(ctx context.Context, id string) error {
 	return nil
 }
 
+// GetDoc fetches a docshelf Document from dynamodb. It will also read and package the Content
+// form an underlying FileStore.
+func (s Store) GetDoc(ctx context.Context, path string) (docshelf.Doc, error) {
+	var doc docshelf.Doc
+
+	if err := s.getItem(ctx, s.docTable, "path", path, &doc); err != nil {
+		return doc, err
+	}
+
+	content, err := s.fs.ReadFile(path)
+	if err != nil {
+		return doc, errors.Wrap(err, "failed to read file from file store")
+	}
+
+	doc.Content = content
+	return doc, nil
+}
+
+// ListDocs fetches a slice of docshelf Document metadata from dynamodb that fit the give prefix
+// and tags supplied.
+func (s Store) ListDocs(ctx context.Context, prefix string, tags ...string) ([]docshelf.Doc, error) {
+	// prefer to filter by tag first if supplied.
+	if len(tags) > 0 {
+		tagged, err := s.listTaggedDocs(ctx, tags)
+		if err != nil {
+			return nil, err
+		}
+
+		// short circuit if no prefix supplied
+		if prefix == "" {
+			return tagged, nil
+		}
+
+		listing := make([]docshelf.Doc, 0, len(tagged))
+		for _, doc := range tagged {
+			if strings.HasPrefix(doc.Path, prefix) {
+				listing = append(listing, doc)
+			}
+		}
+
+		return listing, nil
+	}
+
+	input := dynamodb.ScanInput{
+		TableName: aws.String(s.docTable),
+	}
+
+	res, err := s.client.ScanRequest(&input).Send()
+	if err != nil {
+		return nil, err
+	}
+
+	var docs []docshelf.Doc
+	if err := dyna.UnmarshalListOfMaps(res.Items, &docs); err != nil {
+		return nil, err
+	}
+
+	// TODO (erik): Duplicate of code above. Should refactor to combine.
+	listing := make([]docshelf.Doc, 0, len(docs))
+	for _, doc := range docs {
+		if strings.HasPrefix(doc.Path, prefix) {
+			listing = append(listing, doc)
+		}
+	}
+
+	return listing, nil
+}
+
+func (s Store) listTaggedDocs(ctx context.Context, tags []string) ([]docshelf.Doc, error) {
+	var paths []string
+	for _, t := range tags {
+		var tag Tag
+		if err := s.getItem(ctx, s.tagTable, "tag", t, &tag); err != nil {
+			return nil, err
+		}
+
+		if paths == nil {
+			paths = tag.Paths
+		} else {
+			paths = intersect(paths, tag.Paths)
+		}
+	}
+
+	var docs []docshelf.Doc
+	for _, path := range paths {
+		var doc docshelf.Doc
+		if err := s.getItem(ctx, s.docTable, "path", path, &doc); err != nil {
+			return nil, err
+		}
+
+		docs = append(docs, doc)
+	}
+
+	return docs, nil
+}
+
+// PutDoc creates or updates an existing docshelf Doc in dynamodb. It will also store the
+// Content in an underlying FileStore.
+func (s Store) PutDoc(ctx context.Context, doc docshelf.Doc) error {
+	if doc.Path == "" {
+		return errors.New("can not create a new doc without a path")
+	}
+
+	if _, err := s.GetDoc(ctx, doc.Path); err != nil {
+		if !docshelf.CheckDoesNotExist(err) {
+			return errors.Wrap(err, "could not verify existing file")
+		}
+
+		doc.CreatedAt = time.Now()
+	}
+
+	doc.UpdatedAt = time.Now()
+
+	if err := s.fs.WriteFile(doc.Path, doc.Content); err != nil {
+		return errors.Wrap(err, "failed to write doc to file store")
+	}
+
+	doc.Content = nil // need to clear content before storing doc
+
+	marshaled, err := dyna.MarshalMap(&doc)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal doc for dynamo")
+	}
+
+	input := dynamodb.PutItemInput{
+		TableName: aws.String(s.docTable),
+		Item:      marshaled,
+	}
+
+	if _, err := s.client.PutItemRequest(&input).Send(); err != nil {
+		if err := s.fs.RemoveFile(doc.Path); err != nil { // need to rollback file storage if doc failes
+			return errors.Wrapf(err, "cleanup failed for file: %s", doc.Path)
+		}
+
+		return errors.Wrap(err, "failed to put doc into dynamo")
+	}
+
+	return nil
+}
+
+// A Tag represents the data structure of a tag specific to dynamo.
+type Tag struct {
+	Tag   string   `json:"tag"`
+	Paths []string `json:"paths"`
+}
+
+// TagDoc tags an existing document with the given tags.
+// TODO (erik): This is a mirror of the bolt implementation. Need to research and find out
+// if there's a more efficient way to get this behavior out of dynamo.
+func (s Store) TagDoc(ctx context.Context, path string, tags ...string) error {
+	for _, t := range tags {
+		var tag Tag
+		if err := s.getItem(ctx, s.tagTable, "tag", t, &tag); err != nil {
+			return err
+		}
+
+		// short circuit if the tag alrady contains the path or no tag was returned.
+		if contains(tag.Paths, path) {
+			continue
+		}
+
+		if tag.Tag == "" {
+			tag.Tag = t
+		}
+
+		tag.Paths = append(tag.Paths, path)
+		marshaled, err := dyna.MarshalMap(&tag)
+		if err != nil {
+			return err
+		}
+
+		input := dynamodb.PutItemInput{
+			TableName: aws.String(s.tagTable),
+			Item:      marshaled,
+		}
+
+		if _, err := s.client.PutItemRequest(&input).Send(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// RemoveDoc removes a docshelf Doc from dynamo as well as the underlying FileStore.
+func (s Store) RemoveDoc(ctx context.Context, path string) error {
+	if err := s.fs.RemoveFile(path); err != nil {
+		return errors.Wrap(err, "failed to remove doc from file store")
+	}
+
+	key, err := makeKey("path", path)
+	if err != nil {
+		return errors.Wrap(err, "failed to make key")
+	}
+
+	input := dynamodb.DeleteItemInput{
+		TableName: aws.String(s.docTable),
+		Key:       key,
+	}
+
+	if _, err := s.client.DeleteItemRequest(&input).Send(); err != nil {
+		return errors.Wrap(err, "failed to delete doc from dynamo")
+	}
+
+	return nil
+}
+
 func makeKey(key string, value interface{}) (map[string]dynamodb.AttributeValue, error) {
 	v, err := dyna.Marshal(value)
 	if err != nil {
@@ -211,7 +381,23 @@ func (s Store) ensureTables() error {
 	var ensureErr error
 	go func() {
 		defer wg.Done()
-		if err := ensureTable(s.client, s.userTable, s.userTableInput()); err != nil {
+		if err := ensureTable(s.client, s.userTable, userTableInput(s.userTable)); err != nil {
+			ensureErr = err
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := ensureTable(s.client, s.docTable, docTableInput(s.docTable)); err != nil {
+			ensureErr = err
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := ensureTable(s.client, s.tagTable, tagTableInput(s.tagTable)); err != nil {
 			ensureErr = err
 		}
 	}()
@@ -249,4 +435,115 @@ func makeAttrDef(key string, attrType dynamodb.ScalarAttributeType) dynamodb.Att
 		AttributeType: attrType,
 		AttributeName: aws.String(key),
 	}
+}
+
+func userTableInput(userTable string) dynamodb.CreateTableInput {
+	gsiKey := dynamodb.KeySchemaElement{
+		AttributeName: aws.String("email"),
+		KeyType:       dynamodb.KeyTypeHash,
+	}
+
+	gsi := dynamodb.GlobalSecondaryIndex{
+		IndexName:  aws.String(fmt.Sprintf("%s_email_idx", userTable)),
+		KeySchema:  []dynamodb.KeySchemaElement{gsiKey},
+		Projection: &dynamodb.Projection{ProjectionType: dynamodb.ProjectionTypeKeysOnly},
+	}
+
+	hashKey := dynamodb.KeySchemaElement{
+		AttributeName: aws.String("id"),
+		KeyType:       dynamodb.KeyTypeHash,
+	}
+
+	attrDef := []dynamodb.AttributeDefinition{
+		makeAttrDef("id", dynamodb.ScalarAttributeTypeS),
+		makeAttrDef("email", dynamodb.ScalarAttributeTypeS),
+	}
+
+	return dynamodb.CreateTableInput{
+		TableName:              aws.String(userTable),
+		BillingMode:            dynamodb.BillingModePayPerRequest,
+		AttributeDefinitions:   attrDef,
+		GlobalSecondaryIndexes: []dynamodb.GlobalSecondaryIndex{gsi},
+		KeySchema:              []dynamodb.KeySchemaElement{hashKey},
+	}
+}
+
+func docTableInput(docTable string) dynamodb.CreateTableInput {
+	hashKey := dynamodb.KeySchemaElement{
+		AttributeName: aws.String("path"),
+		KeyType:       dynamodb.KeyTypeHash,
+	}
+
+	attrDef := []dynamodb.AttributeDefinition{
+		makeAttrDef("path", dynamodb.ScalarAttributeTypeS),
+	}
+
+	return dynamodb.CreateTableInput{
+		TableName:            aws.String(docTable),
+		BillingMode:          dynamodb.BillingModePayPerRequest,
+		AttributeDefinitions: attrDef,
+		KeySchema:            []dynamodb.KeySchemaElement{hashKey},
+	}
+}
+
+func tagTableInput(tagTable string) dynamodb.CreateTableInput {
+	hashKey := dynamodb.KeySchemaElement{
+		AttributeName: aws.String("tag"),
+		KeyType:       dynamodb.KeyTypeHash,
+	}
+
+	attrDef := []dynamodb.AttributeDefinition{
+		makeAttrDef("tag", dynamodb.ScalarAttributeTypeS),
+	}
+
+	return dynamodb.CreateTableInput{
+		TableName:            aws.String(tagTable),
+		BillingMode:          dynamodb.BillingModePayPerRequest,
+		AttributeDefinitions: attrDef,
+		KeySchema:            []dynamodb.KeySchemaElement{hashKey},
+	}
+}
+
+func intersect(left, right []string) []string {
+	intersection := make([]string, 0)
+	for _, el := range left {
+		if contains(right, el) {
+			intersection = append(intersection, el)
+		}
+	}
+
+	return intersection
+}
+
+func contains(slice []string, el string) bool {
+	for _, s := range slice {
+		if s == el {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (s Store) getItem(ctx context.Context, table, keyName, key string, out interface{}) error {
+	k, err := makeKey(keyName, key)
+	if err != nil {
+		return err
+	}
+
+	input := dynamodb.GetItemInput{
+		TableName: aws.String(table),
+		Key:       k,
+	}
+
+	res, err := s.client.GetItemRequest(&input).Send()
+	if err != nil {
+		return err
+	}
+
+	if err := dyna.UnmarshalMap(res.Item, out); err != nil {
+		return err
+	}
+
+	return nil
 }
--- dynamo/dynamo_test.go
+++ dynamo/dynamo_test.go
@@ -6,19 +6,21 @@ import (
 	"testing"
 
 	"github.com/eriktate/docshelf"
+	"github.com/eriktate/docshelf/mock"
+	"github.com/rs/xid"
 )
 
 func init() {
-	os.Setenv("DS_DYNAMO_USER_TABLE", "ds_test_user")
-}
+	if err := os.Setenv("DS_DYNAMO_USER_TABLE", "ds_test_user"); err != nil {
+		panic("This should never happen")
+	}
 
-func Test_New(t *testing.T) {
-	// RUN
-	_, err := New()
+	if err := os.Setenv("DS_DYNAMO_DOC_TABLE", "ds_test_doc"); err != nil {
+		panic("This should never happen")
+	}
 
-	// ASSERT
-	if err != nil {
-		t.Fatal(err)
+	if err := os.Setenv("DS_DYNAMO_TAG_TABLE", "ds_test_tag"); err != nil {
+		panic("This should never happen")
 	}
 }
 
@@ -26,7 +28,7 @@ func Test_PutGetRemoveUser(t *testing.T) {
 	// SETUP
 	ctx := context.Background()
 
-	store, err := New()
+	store, err := New(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -85,7 +87,7 @@ func Test_ListUsers(t *testing.T) {
 	// SETUP
 	ctx := context.Background()
 
-	store, err := New()
+	store, err := New(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -128,5 +130,162 @@ func Test_ListUsers(t *testing.T) {
 
 	if len(users) != len(existing)+3 {
 		t.Fatal("returned wrong number of users")
+	}
+}
+
+func Test_PutGetRemoveDoc(t *testing.T) {
+	// SETUP
+	ctx := context.Background()
+
+	store, err := New(mock.NewFileStore())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	doc := docshelf.Doc{
+		Path:      "test.md",
+		Title:     "Test Document",
+		Content:   []byte("This is a test document, for testing purposes only"),
+		CreatedBy: xid.New().String(),
+		UpdatedBy: xid.New().String(),
+	}
+
+	// RUN
+	if err := store.PutDoc(ctx, doc); err != nil {
+		t.Fatal(err)
+	}
+
+	getDoc, err := store.GetDoc(ctx, doc.Path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := store.RemoveDoc(ctx, doc.Path); err != nil {
+		t.Fatal(err)
+	}
+
+	// ASSERT
+	if getDoc.Title != doc.Title {
+		t.Fatal("doc titles don't match")
+	}
+
+	if string(getDoc.Content) != string(doc.Content) {
+		t.Fatal("doc content doesn't match")
+	}
+}
+
+func Test_ListDocs(t *testing.T) {
+	// SETUP
+	ctx := context.Background()
+
+	store, err := New(mock.NewFileStore())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	doc1 := docshelf.Doc{
+		Path:      "test1.md",
+		Title:     "Test Document 1",
+		Content:   []byte("This is a test document, for testing purposes only"),
+		CreatedBy: xid.New().String(),
+		UpdatedBy: xid.New().String(),
+	}
+
+	doc2 := docshelf.Doc{
+		Path:      "test2.md",
+		Title:     "Test Document 2",
+		Content:   []byte("This is a test document, for testing purposes only"),
+		CreatedBy: xid.New().String(),
+		UpdatedBy: xid.New().String(),
+	}
+
+	// RUN
+	if err := store.PutDoc(ctx, doc1); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := store.PutDoc(ctx, doc2); err != nil {
+		t.Fatal(err)
+	}
+
+	list, err := store.ListDocs(ctx, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ASSERT
+	if len(list) != 2 {
+		t.Fatal("listing didn't return enough results")
+	}
+}
+
+func Test_TagLifecycle(t *testing.T) {
+	// SETUP
+	ctx := context.Background()
+
+	store, err := New(mock.NewFileStore())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	doc1 := docshelf.Doc{
+		Path:      "test1.md",
+		Title:     "Test Document 1",
+		Content:   []byte("This is a test document, for testing purposes only"),
+		CreatedBy: xid.New().String(),
+		UpdatedBy: xid.New().String(),
+	}
+
+	doc2 := docshelf.Doc{
+		Path:      "test2.md",
+		Title:     "Test Document 2",
+		Content:   []byte("This is a test document, for testing purposes only"),
+		CreatedBy: xid.New().String(),
+		UpdatedBy: xid.New().String(),
+	}
+
+	// RUN
+	if err := store.PutDoc(ctx, doc1); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := store.PutDoc(ctx, doc2); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := store.TagDoc(ctx, doc1.Path, "test", "one"); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := store.TagDoc(ctx, doc2.Path, "test", "two"); err != nil {
+		t.Fatal(err)
+	}
+
+	testTag, err := store.ListDocs(ctx, "", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	oneTag, err := store.ListDocs(ctx, "", "one")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	twoTag, err := store.ListDocs(ctx, "", "two")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ASSERT
+	if len(testTag) != 2 {
+		t.Fatal("listing didn't return correct number of results")
+	}
+
+	if len(oneTag) != 1 && oneTag[0].Path == doc1.Path {
+		t.Fatal("listing returned wrong results for tag 'one'")
+	}
+
+	if len(twoTag) != 1 && twoTag[0].Path == doc2.Path {
+		t.Fatal("listing returned wrong results for tag 'two'")
 	}
 }
